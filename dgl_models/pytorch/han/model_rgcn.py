d07a4e29be7915437f3e65c24fcb3acd7824c968
Nguyễn Nhật Minh <minhnn@sunshinegroup.vn>
2021-10-12 01:41:20 +0700

    Add simple rgcn for graph classification

---
+++ dgl_models/pytorch/han/model_rgcn.py
@@ -0,0 +1,116 @@
+import os
+
+import torch
+import torch.nn as nn
+from torch.nn.functional import relu
+from torch_geometric.nn import MetaPath2Vec
+import dgl
+import dgl.nn.pytorch as dglnn
+
+from graph_utils import load_hetero_nx_graph, generate_hetero_graph_data, reflect_graph, \
+                         get_number_of_nodes, add_cfg_mapping, get_symmatrical_metapaths, get_node_tracker
+
+
+class RGCN(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
+        super().__init__()
+        self.conv1 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(in_feats, hid_feats)
+            for rel in rel_names}, aggregate='sum')
+        self.conv2 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(hid_feats, out_feats)
+            for rel in rel_names}, aggregate='sum')
+
+    def forward(self, graph, inputs):
+        # inputs is features of nodes
+        h = self.conv1(graph, inputs)
+        h = {k: relu(v) for k, v in h.items()}
+        h = self.conv2(graph, h)
+        return h
+
+
+class RGCNVulClassifier(nn.Module):
+    def __init__(self,compressed_global_graph_path, source_path, feature_extractor=None, node_feature='nodetype', hidden_dim=32, out_size=2, device='cpu'):
+        super(RGCNVulClassifier, self).__init__()
+        self.compressed_global_graph_path = compressed_global_graph_path
+        self.hidden_dim = hidden_dim
+        self.source_path = source_path
+        self.extracted_graph = [f for f in os.listdir(self.source_path) if f.endswith('.sol')]
+        self.filename_mapping = {file: idx for idx, file in enumerate(self.extracted_graph)}
+        self.device = device
+        # Get Global graph
+        nx_graph = load_hetero_nx_graph(compressed_global_graph_path)
+        nx_g_data = generate_hetero_graph_data(nx_graph)
+        _node_tracker = get_node_tracker(nx_graph, self.filename_mapping)
+        self.symmetrical_global_graph_data = reflect_graph(nx_g_data)
+        self.number_of_nodes = get_number_of_nodes(nx_graph)
+        self.symmetrical_global_graph = dgl.heterograph(self.symmetrical_global_graph_data, num_nodes_dict=self.number_of_nodes)
+        self.symmetrical_global_graph.ndata['filename'] = _node_tracker
+        self.meta_paths = get_symmatrical_metapaths(self.symmetrical_global_graph)
+        self.node_types = set([meta_path[0][0] for meta_path in self.meta_paths])
+        self.edge_types = set([meta_path[0][1] for meta_path in self.meta_paths])
+        self.ntypes_dict = {k: v for v, k in enumerate(self.node_types)}
+        features = {}
+        if node_feature == 'nodetype':
+            for ntype in self.symmetrical_global_graph.ntypes:
+                features[ntype] = self._nodetype2onehot(ntype).repeat(self.symmetrical_global_graph.num_nodes(ntype), 1).to(self.device)
+            self.in_size = len(self.node_types)
+        elif node_feature == 'metapath2vec':
+            embedding_dim = 128
+            self.in_size = embedding_dim
+            for metapath in self.meta_paths:
+                _metapath_embedding = MetaPath2Vec(self.symmetrical_global_graph_data, embedding_dim=embedding_dim,
+                        metapath=metapath, walk_length=50, context_size=7,
+                        walks_per_node=5, num_negative_samples=5, num_nodes_dict=None,
+                        sparse=True)
+                ntype = metapath[0][0]
+                if ntype not in features.keys():
+                    features[ntype] = _metapath_embedding(ntype).unsqueeze(0)
+                else:
+                    features[ntype] = torch.cat((features[ntype], _metapath_embedding(ntype).unsqueeze(0)))
+            features = {k: torch.mean(v, dim=0).to(self.device) for k, v in features.items()}
+        
+        self.symmetrical_global_graph = self.symmetrical_global_graph.to(self.device)
+        self.symmetrical_global_graph.ndata['feat'] = features
+
+        self.rgcn = RGCN(self.in_size, self.hidden_dim, self.hidden_dim, self.edge_types)
+        self.classify = nn.Linear(self.hidden_dim, out_size)
+
+    def _nodetype2onehot(self, ntype):
+        feature = torch.zeros(len(self.ntypes_dict), dtype=torch.float)
+        feature[self.ntypes_dict[ntype]] = 1
+        return feature
+
+    def forward(self, batched_graph):
+        batched_graph_embedded = []
+        for g_name in batched_graph:
+            file_ids = self.filename_mapping[g_name]
+            node_mask = {}
+            for node_type in self.node_types:
+                node_mask[node_type] = self.symmetrical_global_graph.ndata['filename'][node_type] == file_ids
+            sub_graph = dgl.node_subgraph(self.symmetrical_global_graph, node_mask)
+            h = sub_graph.ndata['feat']
+            h = self.rgcn(sub_graph, h)
+            graph_embedded = 0
+            for node_type, feature in h.items():
+                graph_embedded += feature.mean(0)
+            if isinstance(graph_embedded, int):
+                batched_graph_embedded.append([0] * self.hidden_dim)
+            else:
+                batched_graph_embedded.append(graph_embedded.tolist())
+        batched_graph_embedded = torch.tensor(batched_graph_embedded).to(self.device)
+        output = self.classify(batched_graph_embedded)
+        return output
+
+
+if __name__ == '__main__':
+    from dataloader import EthIdsDataset
+    from dgl.dataloading import GraphDataLoader
+    dataset = './dataset/aggregate/source_code'
+    compressed_graph = './dataset/call_graph/compressed_graph/compress_call_graphs_no_solidity_calls.gpickle'
+    label = './dataset/aggregate/labels.json'
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    ethdataset = EthIdsDataset(dataset, compressed_graph, label)
+    dataloader = GraphDataLoader(ethdataset, batch_size=128)
+    model = RGCNVulClassifier(compressed_graph, ethdataset.filename_mapping, device=device)
+    model.to(device)
---
+++ dgl_models/pytorch/han/rgcn.py
@@ -0,0 +1,177 @@
+import os
+
+import torch
+import networkx as nx
+from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold
+from dgl.dataloading import GraphDataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from dataloader import EthIdsDataset
+from model_hetero import HANVulClassifier
+from model_rgcn import  RGCNVulClassifier
+from utils import score, accuracy
+from visualization import visualize_average_k_folds
+
+
+def train(args, model, train_loader, optimizer, loss_fcn):
+    model.train()
+    total_accucracy =  0
+    total_macro_f1 = 0
+    total_micro_f1 = 0
+    total_loss = 0
+    circle_lrs = []
+    for idx, (batched_graph, labels) in enumerate(train_loader):
+        labels = labels.to(args['device'])
+        optimizer.zero_grad()
+        logits = model(batched_graph)
+        loss = loss_fcn(logits, labels)
+        train_acc, train_micro_f1, train_macro_f1 = score(logits, labels)
+        loss.backward()
+        optimizer.step()
+        total_accucracy += train_acc
+        total_micro_f1 += train_micro_f1
+        total_macro_f1 += train_macro_f1
+        total_loss += loss.item()
+        circle_lrs.append(optimizer.param_groups[0]["lr"])
+    steps = idx + 1
+    return total_loss/steps, total_micro_f1/steps, train_macro_f1/steps, total_accucracy/steps, circle_lrs
+
+
+def validate(args, model, val_loader, loss_fcn):
+    model.eval()
+    total_loss = 0
+    total_macro_f1 = 0
+    total_micro_f1 = 0
+    total_accucracy =  0
+    with torch.no_grad():
+        for idx, (batched_graph, labels) in enumerate(val_loader):
+            labels = labels.to(args['device'])
+            logits = model(batched_graph)
+            loss = loss_fcn(logits, labels)
+            total_loss += loss.item()
+            val_acc, val_micro_f1, val_macro_f1 = score(logits, labels)
+            total_accucracy += val_acc
+            total_micro_f1 += val_micro_f1
+            total_macro_f1 += val_macro_f1
+    steps = idx + 1
+    return total_loss/steps, total_micro_f1/steps, val_macro_f1/steps, total_accucracy/steps
+
+
+def main(args):
+    epochs = args['num_epochs']
+    k_folds = args['k_folds']
+    device = args['device']
+    ethdataset = EthIdsDataset(args['dataset'], args['compressed_graph'], args['label'])
+    kfold = KFold(n_splits=k_folds, shuffle=True)
+    train_results = {}
+    val_results = {}
+    # Get feature extractor
+    print('Getting features')
+    if args['node_feature'] == 'han':
+        han_model = HANVulClassifier(args['feature_compressed_graph'], ethdataset.filename_mapping, node_feature='metapath2vec', hidden_size=16, device=args['device'])
+        han_model.load_state_dict(torch.load(args['feature_extractor']))
+        han_model.to(args['device'])
+        han_model.eval()
+    else:
+        han_model = None
+
+    nx_graph = nx.read_gpickle(args['compressed_graph'])
+    number_of_nodes = len(nx_graph)
+    for fold, (train_ids, val_ids) in enumerate(kfold.split(range(number_of_nodes))):
+        train_results[fold] = {'loss': [], 'acc': [], 'micro_f1': [], 'macro_f1': [], 'lrs': []}
+        val_results[fold] = {'loss': [], 'acc': [], 'micro_f1': [], 'macro_f1': []}
+        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+        val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+        train_dataloader = GraphDataLoader(ethdataset,batch_size=args['batch_size'],drop_last=False,sampler=train_subsampler)
+        val_dataloader = GraphDataLoader(ethdataset,batch_size=args['batch_size'],drop_last=False,sampler=val_subsampler)
+        print('Start training fold {} with {}/{} train/val smart contracts'.format(fold, len(train_subsampler), len(val_subsampler)))
+        total_steps = epochs
+        model = RGCNVulClassifier(args['compressed_graph'], args['dataset'], feature_extractor=han_model, node_feature=args['node_feature'], device=device)
+        model.to(device)
+        loss_fcn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, total_steps=total_steps)
+        lrs = []
+        for epoch in range(epochs):
+            print('Fold {} - Epochs {}'.format(fold, epoch))
+            train_loss, train_micro_f1, train_macro_f1, train_acc, lrs = train(args, model, train_dataloader, optimizer, loss_fcn)
+            print('Train Loss: {:.4f} | Train Micro f1: {:.4f} | Train Macro f1: {:.4f} | Train Accuracy: {:.4f}'.format(
+                    train_loss, train_micro_f1, train_macro_f1, train_acc))
+            val_loss, val_micro_f1, val_macro_f1, val_acc = validate(args, model, val_dataloader, loss_fcn)
+            print('Val Loss:   {:.4f} | Val Micro f1:   {:.4f} | Val Macro f1:   {:.4f} | Val Accuracy:   {:.4f}'.format(
+                    val_loss, val_micro_f1, val_macro_f1, val_acc))
+            scheduler.step()
+            train_results[fold]['loss'].append(train_loss)
+            train_results[fold]['micro_f1'].append(train_micro_f1)
+            train_results[fold]['macro_f1'].append(train_macro_f1)
+            train_results[fold]['acc'].append(train_acc)
+            train_results[fold]['lrs'] += lrs
+
+            val_results[fold]['loss'].append(val_loss)
+            val_results[fold]['micro_f1'].append(val_micro_f1)
+            val_results[fold]['macro_f1'].append(val_macro_f1)
+            val_results[fold]['acc'].append(val_acc)
+
+        print('Saving model fold {}'.format(fold))
+        save_path = os.path.join(args['output_models'], f'han_fold_{fold}.pth')
+        torch.save(model.state_dict(), save_path)
+    return train_results, val_results
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser('HAN')
+    parser.add_argument('-s', '--seed', type=int, default=1,
+                        help='Random seed')
+    parser.add_argument('-ld', '--log-dir', type=str, default='./logs/ijcai2020_smartbugs', help='Dir for saving training results')
+    parser.add_argument('--compressed_graph', type=str, default='./dataset/call_graph/compressed_graph/compress_call_graphs_no_solidity_calls.gpickle')
+    parser.add_argument('--dataset', type=str, default='./dataset/aggregate/source_code')
+    parser.add_argument('--label', type=str, default='./dataset/aggregate/labels.json')
+    parser.add_argument('--output_models', type=str, default='./models/call_graph_rgcn')
+    parser.add_argument('--checkpoint', type=str, default='./models/call_graph_rgcn/han_fold_1.pth')
+    parser.add_argument('--feature_compressed_graph', type=str, default='./dataset/aggregate/compressed_graph/compressed_graphs.gpickle')
+    parser.add_argument('--feature_extractor', type=str, default='./models/metapath2vec_cfg/han_fold_1.pth')
+    parser.add_argument('--node_feature', type=str, default='metapath2vec')
+    parser.add_argument('--k_folds', type=int, default=5)
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--non_visualize', action='store_true')
+    args = parser.parse_args().__dict__
+
+    default_configure = {
+    'lr': 0.0005,             # Learning rate
+    'num_heads': 8,        # Number of attention heads for node-level attention
+    'hidden_units': 8,
+    'dropout': 0.6,
+    'weight_decay': 0.001,
+    'num_epochs': 100,
+    'batch_size': 256,
+    'patience': 100,
+    'device': 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    }
+    args.update(default_configure)
+    torch.manual_seed(args['seed'])
+
+    if not os.path.exists(args['output_models']):
+        os.makedirs(args['output_models'])
+
+    # Training
+    if not args['test']:
+        print('Training phase')
+        train_results, val_results = main(args)
+        if not args['non_visualize']:
+            print('Visualizing')
+            visualize_average_k_folds(args, train_results, val_results)
+    # Testing
+    # else:
+    #     print('Testing phase')
+    #     ethdataset = EthIdsDataset(args['dataset'], args['compressed_graph'], args['label'])
+    #     smartbugs_ids = [ethdataset.filename_mapping[sc] for sc in os.listdir(args['testset'])]
+    #     test_dataloader = GraphDataLoader(ethdataset, batch_size=8, drop_last=False, sampler=smartbugs_ids)
+    #     model = RGCNVulClassifier(args['compressed_graph'], ethdataset.filename_mapping, hidden_size=16, out_size=2,num_heads=8, dropout=0.6, device=args['device'])
+    #     model.load_state_dict(torch.load(args['checkpoint']))
+    #     model.to(args['device'])
+    #     test_micro_f1, test_macro_f1, test_acc = test(args, model, test_dataloader)
+    #     print('Test Micro f1:   {:.4f} | Test Macro f1:   {:.4f} | Test Accuracy:   {:.4f}'.format(test_micro_f1, test_macro_f1, test_acc))
