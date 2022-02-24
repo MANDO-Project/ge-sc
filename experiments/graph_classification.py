1f211bb5b285e86e3264ea51c4b7c50996309cde
Nguyễn Nhật Minh <minhnn@sunshinegroup.vn>
2022-02-24 16:12:33 +0700

    Update graph classifier inspect scripts

--- experiments/graph_classification.py
+++ experiments/graph_classification.py
@@ -1,33 +1,36 @@
 import os
-import sys
+import argparse
 from os.path import join
 
 import pickle
 import json
-import numpy as np
 import networkx as nx
 import torch
-from torch.utils.tensorboard import SummaryWriter
-from torch.nn.functional import cross_entropy
 from tabulate import tabulate
-from sklearn.metrics import f1_score, precision_recall_fscore_support
-from sklearn.model_selection import KFold, train_test_split
-from dgl.dataloading import GraphDataLoader
-from zmq import device
-from torch_geometric.nn import MetaPath2Vec
+from sklearn.model_selection import train_test_split
 from statistics import mean
 
-from sco_models.dataloader import EthIdsDataset
-from sco_models.model_hetero import HAN, HANVulClassifier
-from sco_models.model_metapath2vec import VulMetaPath2Vec
-from sco_models.visualization import visualize_average_k_folds, visualize_k_folds
-from sco_models.utils import score, get_classification_report, get_confusion_matrix
+from sco_models.model_hetero import MANDOGraphClassifier
+from sco_models.utils import get_classification_report
 from sco_models.graph_utils import reveert_map_node_embedding, load_hetero_nx_graph
 
-ROOT = '.'
+
+# Arguments
+parser = argparse.ArgumentParser('MANDO Experiments')
+parser.add_argument('-s', '--seed', type=int, default=1,
+                    help='Random seed')
+parser.add_argument('-e', '--epochs', type=int, default=2,
+                    help='Random seed')
+parser.add_argument('-rep', '--repeat', type=int, default=2,
+                    help='Random seed')
+parser.add_argument('-r', '--result', action='store_true')
+args = parser.parse_args().__dict__
+
+torch.manual_seed(args['seed'])
+ROOT = './experiments'
 DATA_ID = 0
-REPEAT = 20
-EPOCHS = 50
+REPEAT = args['repeat']
+EPOCHS = args['epochs']
 TASK = "graph_classification"
 STRUCTURE = 'han'
 COMPRESSED_GRAPH = 'cfg_cg'
@@ -37,13 +40,15 @@ VAL_RATE = 0.3
 ratio = 1
 
 
-models = ['nodetype', 'metapath2vec', 'gae', 'line', 'node2vec']
+models = ['base_metapath2vec', 'base_gae', 'base_line', 'base_node2vec', 'nodetype', 'metapath2vec', 'gae', 'line', 'node2vec']
 bug_list = ['access_control', 'arithmetic', 'denial_of_service',
             'front_running', 'reentrancy', 'time_manipulation', 
             'unchecked_low_level_calls']
 file_counter = {'access_control': 57, 'arithmetic': 60, 'denial_of_service': 46,
               'front_running': 44, 'reentrancy': 71, 'time_manipulation': 50, 
               'unchecked_low_level_calls': 95}
+print(f'Run experiments on {len(bug_list)} kinds of bug for {len(models)} kinds of model.')
+print(f'Repeat {REPEAT} times and {EPOCHS} epochs for each experiment.')
 
 
 def train(model, train_loader, labels, device):
@@ -78,7 +83,7 @@ def base_metapath2vec(compressed_graph, source_path, file_name_dict, dataset, bu
     if not os.path.exists(logs):
         os.makedirs(logs)
     output_models = f'{ROOT}/models/{TASK}/{STRUCTURE}/{COMPRESSED_GRAPH}/base_metapath2vec/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}/'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=None, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=None, 
                                  node_feature='metapath2vec', device=device)
     features = model.symmetrical_global_graph.ndata['feat']
     nx_graph = load_hetero_nx_graph(compressed_graph)
@@ -277,7 +282,7 @@ def nodetype(compressed_graph, source_path, dataset, bugtype, device):
         os.makedirs(output_models)
     feature_extractor = None
     node_feature = 'nodetype'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
                                  node_feature=node_feature, device=device)
     model.reset_parameters()
     model.to(device)
@@ -310,7 +315,7 @@ def metapath2vec(compressed_graph, source_path, dataset, bugtype, device):
         os.makedirs(output_models)
     feature_extractor = None
     node_feature = 'metapath2vec'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
                                  node_feature=node_feature, device=device)
     model.reset_parameters()
     model.to(device)
@@ -343,7 +348,7 @@ def gae(compressed_graph, source_path, dataset, bugtype, device):
         os.makedirs(output_models)
     feature_extractor = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_gae_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
     node_feature = 'gae'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
                                  node_feature=node_feature, device=device)
     model.reset_parameters()
     model.to(device)
@@ -376,7 +381,7 @@ def line(compressed_graph, source_path, dataset, bugtype, device):
         os.makedirs(output_models)
     feature_extractor = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_line_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
     node_feature = 'line'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
                                  node_feature=node_feature, device=device)
     model.reset_parameters()
     model.to(device)
@@ -409,7 +414,7 @@ def node2vec(compressed_graph, source_path, dataset, bugtype, device):
         os.makedirs(output_models)
     feature_extractor = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_node2vec_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
     node_feature = 'node2vec'
-    model = HANVulClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
+    model = MANDOGraphClassifier(compressed_graph, source_path, feature_extractor=feature_extractor, 
                                  node_feature=node_feature, device=device)
     model.reset_parameters()
     model.to(device)
@@ -438,14 +443,11 @@ def main(device):
         print('Bugtype {}'.format(bugtype))
         for i in range(REPEAT):
             print(f'Train bugtype {bugtype} {i}-th')
-            # compressed_graph = f'{ROOT}/ge-sc-data/{TASK}/{COMPRESSED_GRAPH}/{bugtype}/{DATASET}/compressed_graphs.gpickle'
-            # label = f'{ROOT}/ge-sc-data/{TASK}/{COMPRESSED_GRAPH}/{bugtype}/{DATASET}/graph_labels.json'
-            # source_path = f'{ROOT}/ge-sc-data/{TASK}/{COMPRESSED_GRAPH}/{bugtype}/{DATASET}'
             compressed_graph = f'{ROOT}/ge-sc-data/source_code/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}/{COMPRESSED_GRAPH}_compressed_graphs.gpickle'
             nx_graph = nx.read_gpickle(compressed_graph)
             file_name_dict = get_node_id_by_file_name(nx_graph)
             label = f'{ROOT}/ge-sc-data/source_code/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}/graph_labels.json'
-            source_path = f'{ROOT}/ge-sc-data/graph_classification/cg/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}'
+            source_path = f'{ROOT}/ge-sc-data/source_code/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}'
             with open(label, 'r') as f:
                 annotations = json.load(f)
             total_files = [f for f in os.listdir(source_path) if f.endswith('.sol')]
@@ -462,23 +464,25 @@ def main(device):
             X_train, X_val, y_train, y_val = train_test_split(total_files, targets, train_size=TRAIN_RATE)
             dataset = (tuple(X_train), tuple(X_val), y_train, y_val)
             print('Start training with {}/{} train/val smart contracts'.format(len(X_train), len(X_val)))
-            # Run experiments
-            # nodetype(compressed_graph, source_path, dataset, bugtype, device)
-            # metapath2vec(compressed_graph, source_path, dataset, bugtype, device)
-            # if bugtype not in ['arithmetic', 'front_running', 'reentrancy', 'unchecked_low_level_calls']:
-            #     gae(compressed_graph, source_path, dataset, bugtype, device)
-            # line(compressed_graph, source_path, dataset, bugtype, device)
-            # node2vec(compressed_graph, source_path, dataset, bugtype, device)
-
-            ## Base lines
             gae_embedded = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_gae_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
             line_embedded = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_line_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
             node2vec_embedded = f'{ROOT}/ge-sc-data/source_code/gesc_matrices_node_embedding/matrix_node2vec_dim128_of_core_graph_of_{bugtype}_{COMPRESSED_GRAPH}_clean_{file_counter[bugtype]}_{DATA_ID}.pkl'
+
+            # Run experiments
+            ## Base lines
             base_metapath2vec(compressed_graph, source_path, file_name_dict, dataset, bugtype, device)
             if bugtype not in ['arithmetic', 'front_running', 'reentrancy', 'unchecked_low_level_calls']:
                 base_gae(dataset, bugtype, gae_embedded, file_name_dict, device)
             base_line(dataset, bugtype, line_embedded, file_name_dict, device)
             base_node2vec(dataset, bugtype, node2vec_embedded, file_name_dict, device)
+
+            ## Out models
+            nodetype(compressed_graph, source_path, dataset, bugtype, device)
+            metapath2vec(compressed_graph, source_path, dataset, bugtype, device)
+            if bugtype not in ['arithmetic', 'front_running', 'reentrancy', 'unchecked_low_level_calls']:
+                gae(compressed_graph, source_path, dataset, bugtype, device)
+            line(compressed_graph, source_path, dataset, bugtype, device)
+            node2vec(compressed_graph, source_path, dataset, bugtype, device)
 
 
 def get_avg_results(report_path, top_rate=0.5):
@@ -506,15 +510,13 @@ def get_max_results(report_path):
 def get_results():
     buggy_f1_report = {}
     macro_f1_report = {}
-    models = ['base_metapath2vec', 'base_gae', 'base_line', 'base_node2vec']
     for bugtype in bug_list:
         for model in models:
-            # report_path = f'{ROOT}/logs/{TASK}/{STRUCTURE}/{COMPRESSED_GRAPH}/{DATASET}/{model}/{bugtype}/test_report.json'
-            # report_path = f'{ROOT}/logs/{TASK}/{STRUCTURE}/{COMPRESSED_GRAPH}/{model}/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{1}/test_report.json'
             if  model in ['gae', 'base_gae'] and bugtype in ['arithmetic', 'front_running', 'reentrancy', 'unchecked_low_level_calls']:
-                continue
-            report_path = f'{ROOT}/logs/{TASK}/{STRUCTURE}/{COMPRESSED_GRAPH}/{model}/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}/test_report.json'
-            buggy_f1, macro_f1 = get_avg_results(report_path, top_rate=0.5)
+                buggy_f1, macro_f1 = '-', '-'
+            else:
+                report_path = f'{ROOT}/logs/{TASK}/{STRUCTURE}/{COMPRESSED_GRAPH}/{model}/{bugtype}/clean_{ratio*file_counter[bugtype]}_buggy_curated_{DATA_ID}/test_report.json'
+                buggy_f1, macro_f1 = get_avg_results(report_path, top_rate=0.5)
             # buggy_f1, macro_f1 = get_max_results(report_path)
             if model not in buggy_f1_report:
                 buggy_f1_report[model] = [buggy_f1]
@@ -522,28 +524,29 @@ def get_results():
             else:
                 buggy_f1_report[model].append(buggy_f1)
                 macro_f1_report[model].append(macro_f1)
+    data = []
     for model in models:
-        # print('& ', end=' ')
-        # print('\%  & '.join(['%.2f'%n for n in buggy_f1_report[model]]), end=r' \\')
+        # print(' ', end=' ')
+        # print(' \t'.join(['%.2f'%n for n in buggy_f1_report[model]]), end=r'')
         # print()
-        # print('& ', end=' ')
-        # print('\%  & '.join(['%.2f'%n for n in macro_f1_report[model]]), end=r' \\')
+        # print(' ', end=' ')
+        # print(' \t'.join(['%.2f'%n for n in macro_f1_report[model]]), end=r'')
         # print()
-
-        print(' ', end=' ')
-        print(' \t'.join(['%.2f'%n for n in buggy_f1_report[model]]), end=r'')
-        print()
-        print(' ', end=' ')
-        print(' \t'.join(['%.2f'%n for n in macro_f1_report[model]]), end=r'')
-        print()
+        buggy_f1_row = []
+        macro_f1_row = []
+        for i in range(len(buggy_f1_report[model])):
+            buggy_f1 = buggy_f1_report[model][i]
+            macro_f1 = macro_f1_report[model][i]
+            buggy_f1_row.append('%.2f'%buggy_f1 + '%' if isinstance(buggy_f1, float) else buggy_f1)
+            macro_f1_row.append('%.2f'%macro_f1 + '%' if isinstance(macro_f1, float) else macro_f1)
+        data.append([model, 'Buggy-F1'] + buggy_f1_row)
+        data.append([model, 'Macro-F1'] + macro_f1_row)
+    print(tabulate(data, headers=bug_list, tablefmt='orgtbl'))
 
 
 if __name__ == '__main__':
-    torch.manual_seed(1)
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'result':
+    if args['result']:
             get_results()
     else:
         main(device)
