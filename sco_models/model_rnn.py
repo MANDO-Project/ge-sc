a99cd61e02d6f625ca86d29d8a57483d8edc7d8a
MinhNN <nguyenminh180798@gmail.com>
2022-07-04 15:47:16 +0800

    Update RNN model

--- sco_models/model_rnn.py
+++ sco_models/model_rnn.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from os.path import join
 
+import argparse
 import numpy as np
 import json
 import matplotlib
@@ -16,7 +17,6 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_se
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-# from data import PaddedTensorDataset
 
 # matplotlib.use('Agg')
 
@@ -28,7 +28,6 @@ elif torch.backends.mps.is_available():
     DEVICE = torch.device("mps")
 else:
     DEVICE = 'cpu'
-
 # Hyper-parameters
 BYTECODE = 'runtime'
 MODEL = 'hgt'
@@ -41,6 +40,32 @@ num_layers = 2
 num_classes = 10
 learning_rate = 0.01
 max_epochs = 25
+
+
+class PaddedTensorDataset(Dataset):
+    """Dataset wrapping data, target and length tensors.
+
+    Each sample will be retrieved by indexing both tensors along the first
+    dimension.
+
+    Arguments:
+        data_tensor (Tensor): contains sample data.
+        target_tensor (Tensor): contains sample targets (labels).
+        length (Tensor): contains sample lengths.
+        raw_data (Any): The data that has been transformed into tensor, useful for debugging
+    """
+
+    def __init__(self, data_tensor, target_tensor, length_tensor):
+        assert data_tensor.size(0) == target_tensor.size(0) == length_tensor.size(0)
+        self.data_tensor = data_tensor
+        self.target_tensor = target_tensor
+        self.length_tensor = length_tensor
+
+    def __getitem__(self, index):
+        return self.data_tensor[index], self.target_tensor[index], self.length_tensor[index]
+
+    def __len__(self):
+        return self.data_tensor.size(0)
 
 
 class PaddedTensorDataset(Dataset):
@@ -172,65 +197,53 @@ if __name__ == '__main__':
               'unchecked_low_level_calls': 95}
     BYTECODE = opt.bytecode
     for bug, count in bug_type.items():
-        output_report = f'./experiments/logs/graph_classification/byte_code/smartbugs/{BYTECODE}/{MODEL}/cfg/base_lstm/{bug}'
-        if not os.path.exists(output_report):
-            os.makedirs(output_report)
-        for i in range(REAPEAT):
-            annotation_path = f'./experiments/ge-sc-data/byte_code/smartbugs/contract_labels/{bug}/{BYTECODE}_balanced_contract_labels.json'
-            with open(annotation_path, 'r') as f:
-                annotation = json.load(f)
-            evm = [anno['contract_name'] for anno in annotation]
-            targets = [anno['targets'] for anno in annotation]
-            targets = torch.tensor(targets, device=DEVICE)
-            evm_source = []
-            evm_lenghts = []
-            evm_path = f'./experiments/ge-sc-data/byte_code/smartbugs/{BYTECODE}/evm/{bug}/clean_{count}_buggy_curated_0'
-            for source in evm:
-                with open(join(evm_path, source.replace('.gpickle', '.evm')), 'r') as f:
-                    evm = f.read()
-                evm_dec = torch.tensor([int(i, 16) for i in evm], device=DEVICE)
-                evm_source.append(evm_dec)
-                evm_lenghts.append(evm_dec.shape[0])
-            evm_padded = pad_sequence(evm_source, batch_first=True)
-            evm_lenghts = torch.tensor(evm_lenghts)
-            evm_train, evm_test, target_train, target_test, len_train, len_test = train_test_split(evm_padded, targets, evm_lenghts, train_size=TRAIN_SIZE)
+        
+        annotation_path = f'./experiments/ge-sc-data/byte_code/smartbugs/contract_labels/{bug}/{BYTECODE}_balanced_contract_labels.json'
+        with open(annotation_path, 'r') as f:
+            annotation = json.load(f)
+        evm = [anno['contract_name'] for anno in annotation]
+        targets = [anno['targets'] for anno in annotation]
+        targets = torch.tensor(targets, device=DEVICE)
+        evm_source = []
+        evm_lenghts = []
+        evm_path = f'./experiments/ge-sc-data/byte_code/smartbugs/{BYTECODE}/evm/{bug}/clean_{count}_buggy_curated_0'
+        for source in evm:
+            with open(join(evm_path, source.replace('.gpickle', '.evm')), 'r') as f:
+                evm = f.read()
+            evm_dec = torch.tensor([int(i, 16) for i in evm], device=DEVICE)
+            evm_source.append(evm_dec)
+            evm_lenghts.append(evm_dec.shape[0])
+        evm_padded = pad_sequence(evm_source, batch_first=True)
+        evm_lenghts = torch.tensor(evm_lenghts)
+        evm_train, evm_test, target_train, target_test, len_train, len_test = train_test_split(evm_padded, targets, evm_lenghts, train_size=TRAIN_SIZE)
 
-            # Build model
-            model = LSTMClassifier(16, 128, 128, 2)
-            model = model.to(DEVICE)
-            optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-            criterion = nn.NLLLoss()
-            evm_train_dataloader = DataLoader(PaddedTensorDataset(evm_train, target_train, len_train), batch_size=BATCH_SIZE)
-            evm_test_dataloader = DataLoader(PaddedTensorDataset(evm_test, target_test, len_test), batch_size=BATCH_SIZE)
-            print('Bug type: {}'.format(bug))
-            for epoch in range(max_epochs):
-                # print('Epoch:', epoch)
-                y_true = list()
-                y_pred = list()
-                total_loss = 0
-                for idx, (evm, target, lens) in enumerate(evm_train_dataloader):
-                    # target = target.cpu()
-                    model.zero_grad()
-                    pred = model(evm, lens.cpu().numpy())
-                    loss = criterion(pred, target)
-                    loss.backward()
-                    optimizer.step()
-                    pred_idx = torch.max(pred, 1)[1]
-                    y_true += list(target.cpu().numpy())
-                    y_pred += list(pred_idx.cpu().numpy())
-                    total_loss += loss
-                acc = accuracy_score(y_true, y_pred)
-            val_loss, val_acc, class_report = evaluate_validation_set(model, evm_test_dataloader, criterion)
-            print(output_report)
-            if os.path.isfile(join(output_report, 'test_report.json')):
-                with open(join(output_report, 'test_report.json'), 'r') as f:
-                    report = json.load(f)
-                report.append(class_report)
-            else:
-                report = [class_report]
-            with open(join(output_report, 'test_report.json'), 'w') as f:
-                json.dump(report, f, indent=2)
-            # print("Train acc:      {} - loss: {} \nValidation acc: {} - loss: {}".format(acc, total_loss.data.float()/len(evm_train_dataloader), val_acc, val_loss))
+        # Build model
+        model = LSTMClassifier(16, 128, 128, 2)
+        model = model.to(DEVICE)
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+        criterion = nn.NLLLoss()
+        evm_train_dataloader = DataLoader(PaddedTensorDataset(evm_train, target_train, len_train), batch_size=BATCH_SIZE)
+        evm_test_dataloader = DataLoader(PaddedTensorDataset(evm_test, target_test, len_test), batch_size=BATCH_SIZE)
+        print('Bug type: {}'.format(bug))
+        for epoch in range(max_epochs):
+            # print('Epoch:', epoch)
+            y_true = list()
+            y_pred = list()
+            total_loss = 0
+            for idx, (evm, target, lens) in enumerate(evm_train_dataloader):
+                # target = target.cpu()
+                model.zero_grad()
+                pred = model(evm, lens.cpu().numpy())
+                loss = criterion(pred, target)
+                loss.backward()
+                optimizer.step()
+                pred_idx = torch.max(pred, 1)[1]
+                y_true += list(target.cpu().numpy())
+                y_pred += list(pred_idx.cpu().numpy())
+                total_loss += loss
+            acc = accuracy_score(y_true, y_pred)
+        val_loss, val_acc = evaluate_validation_set(model, evm_test_dataloader, criterion)
+        print("Train acc:      {} - loss: {} \nValidation acc: {} - loss: {}".format(acc, total_loss.data.float()/len(evm_train_dataloader), val_acc, val_loss))
 
         # # build the creation model
         # seq = Sequence()
